afdd03742c30f366cddb4484e28be2e63f428da2
rllee <rllee@gate.sinica.edu.tw>
2014-01-22 16:26:54 +0800

    new class

---
+++ Testee/src/edu/sinica/testee/OtherActivity.java
@@ -0,0 +1,13 @@
+package edu.sinica.testee;
+
+public class OtherActivity {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
